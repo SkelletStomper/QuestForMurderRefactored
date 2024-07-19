5f1a448f38de8e973a414b0450c1d8627cecf2b5
Olivia Haufschild <SkelletStomper@gmx.de>
2024-07-19 13:02:03 +0200

    feat: Dice library

---
+++ dice.py
@@ -0,0 +1,28 @@
+import random
+
+
+def d(sides: int):
+    return random.randint(1, sides)
+class Dice:
+    def __init__(self, sides=6):
+        self.sides = sides
+
+    def roll(self):
+        return random.randint(1, self.sides)
+
+
+
+
+
+
+class DiceSet:
+    def __init__(self, die=None):
+        if die is None:
+            die = {}
+
+        self.die: list[Dice] = []
+        for sides, times in die.items():
+            self.die += [Dice(sides) for _ in range(times)]
+
+    def roll(self):
+        return sum([dice.roll() for dice in self.die])
