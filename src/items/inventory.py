f45a05c11cca84a1b4f91e7a5917e5b5be6dc5cb
Olivia Haufschild <SkelletStomper@gmx.de>
2024-07-31 13:32:04 +0200

    feat: is_equipable and apply_effects for weaponSlot and armorSlot

--- src/items/inventory.py
+++ src/items/inventory.py
@@ -1,8 +1,11 @@
-
+from src.combat.attack import Attack
+from src.items.items import Item, Weapon, Armor
 
 class EquipEffects:
     def __init__(self):
         self.armor = 0
+
+        self.granted_attacks: list[Attack] = 0
 
 
 class EquipSlot:
@@ -10,17 +13,17 @@ class EquipSlot:
         self._item = None
 
     def free(self) -> bool:
-        return self.item is None
+        return self._item is None
 
-    def is_equipable(self, item) -> bool:
+    def is_equipable(self, item: Item) -> bool:
         pass
 
-    def equip(self, item) -> None:
+    def equip(self, item: Item) -> None:
         if self._item is not None:
             raise RuntimeError("Cannot equip another item while there is already one equipped!")
         self._item = item
 
-    def unequip(self):
+    def unequip(self) -> Item:
         item = self._item
         self._item = None
         return item
@@ -30,7 +33,20 @@ class EquipSlot:
 
 
 class WeaponSlot(EquipSlot):
-    pass
+    def is_equipable(self, item: Item) -> bool:
+        return isinstance(item, Weapon)
+
+    def apply_bonus(self, equip_effects: EquipEffects) -> EquipEffects:
+        weapon: Weapon = self._item
+        attack = Attack(
+            dmg=weapon.damage,
+            accuracy=0,
+            crt=1.0,
+            types=weapon.types
+        )
+        equip_effects.granted_attacks.append(attack)
+        return equip_effects
+
 
 
 class OffhandSlot(EquipSlot):
@@ -38,8 +54,15 @@ class OffhandSlot(EquipSlot):
 
 
 class ArmorSlot(EquipSlot):
-    pass
+    def is_equipable(self, item: Item) -> bool:
+        return isinstance(item, Armor)
 
+    def apply_bonus(self, equip_effects: EquipEffects) -> EquipEffects:
+        armor: Armor = self._item
+
+        equip_effects.armor += armor.armor
+
+        return equip_effects
 
 
 class Inventory:
