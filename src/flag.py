32c495a8c1f2c72644cc07c48f75f269d79c3b6b
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-01 11:14:01 +0200

    feat: copying flags with new values

--- src/flag.py
+++ src/flag.py
@@ -1,22 +1,35 @@
 from src.combat.attack import WeaknessSet
 
 import json
+from copy import copy
 
 
 class Flag:
-    def __init__(self, name: str, in_dict: dict):
-        if "$" in name:
-            name, self.value = name.split("$")
-        else:
-            self.value = 0
-        self.name = name
+    def __init__(self, name: str, init_dict: dict):
 
-        self.description: str = in_dict["description"]
+        self.name = name
+        self.value = None
+        self.value_type = None
+        if "default_value" in init_dict:
+            self.value = init_dict["default_value"]
+            self.value_type = type(self.value)
+
+        self.description: str = init_dict["description"]
 
         raw_weaknesses = {}
-        if "weaknesses" in in_dict.values():
-            raw_weaknesses = in_dict["weaknesses"]
+        if "weaknesses" in init_dict.values():
+            raw_weaknesses = init_dict["weaknesses"]
         self.weaknesses = WeaknessSet(raw_weaknesses)
+
+    def get_copy(self, value=None):
+        flag_copy = copy(self)
+        if value is not None:
+            flag_copy.value = value
+
+        return flag_copy
+
+    def __eq__(self, value: str):
+        return self.name == value
 
 
 class FlagProvider:
@@ -31,7 +44,17 @@ class FlagProvider:
     def __getitem__(self, value) -> Flag:
         return self.flags[value]
 
+    def __call__(self, flag_str: str):
+        flag_name = flag_str
+        value = None
+        if "$" in flag_str:
+            flag_name, value = flag_str.split("$")
+            value = self[flag_name].value_type(value)
 
-flagProvider = FlagProvider()
+        return self[flag_name].get_copy(value)
+
+
+
+flag_provider = FlagProvider()
 
 
