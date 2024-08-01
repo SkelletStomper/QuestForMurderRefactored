909b0e66b337c4241d8bcb7b98d04f1eddf65b60
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-01 20:56:18 +0200

    refactor: LString initialization

--- src/localization/l_string.py
+++ src/localization/l_string.py
@@ -8,19 +8,22 @@ class LString:
         lstr = lstr.replace("{atk.", "{attacking.")
         lstr = lstr.replace("{def.", "{defending.")
         lstr = lstr.replace("{pl.", "{player.")
-        if not self.les_allowed():
-            raise ValueError(f"LString {lstr} contains forbidden les!")
-        self.lstr = lstr
+
+        self._lstr = lstr
+
+        self.les_allowed()
 
     def les_allowed(self):
         allowed_les = ["attacking", "defending", "player"]
         le_pattern = r"{[a-zA-Z0-9\.\_]*}"
-        les_used = re.findall(self.lstr, le_pattern)
+        les_used = re.findall(self._lstr, le_pattern)
         les_used = set({le[1:-1].split(".")[0] for le in les_used})
 
         for le in les_used:
             if le not in allowed_les:
-                return False
+                lstr = self._lstr
+                self._lstr = None
+                raise ValueError(f"LString {lstr} contains forbidden les!")
         return True
 
     def parse(
@@ -29,7 +32,7 @@ class LString:
             defending: LocalizedEntity | None = None,
             ) -> str:
 
-        return self.lstr.format(**locals())
+        return self._lstr.format(**locals())
 
 
 
