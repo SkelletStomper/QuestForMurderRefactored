303630964516a3fd40cfa11621483cd2961f8cfe
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-02 08:59:09 +0200

    fix: tests can now read the data

--- src/util/read_data.py
+++ src/util/read_data.py
@@ -1,7 +1,6 @@
 import json
 
 import os
-from pathlib import Path
 from typing import Any
 
 
@@ -9,12 +8,15 @@ def read_json_data(filename: str) -> dict[str, Any]:
     cwd = os.getcwd()
 
     highest_dir = cwd.split(os.sep)[-1]
-    access_name = ""
+
     if highest_dir == "src":
         access_name = f"data/{filename}"
     elif highest_dir == "QuestForMurderRefactored":
         access_name = f"src/data/{filename}"
+    elif highest_dir == "tests":
+        access_name = f"../src/data/{filename}"
+    else:
+        print(cwd)
+        raise RuntimeError("read_json_data does not recognize the cwd")
     with open(access_name, "r") as f:
         return json.load(f)
-
-
