d4ea41ac5636ecf9d1987980338fa2df738bd39c
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-02 08:51:21 +0200

    style: restructuring of attack class

--- src/combat/attack.py
+++ src/combat/attack.py
@@ -20,28 +20,6 @@ class AttackType(Enum):
     DARKNESS = 'DARKNESS'
 
 
-class Attack:
-    def __init__(self,
-                 dmg: int,
-                 acc: int = 0,
-                 crt: float = 1.0,
-                 types: list[AttackType] = None,
-                 atk_str: LString | None = None
-                 ) -> None:
-
-        if types is None:
-            types = [AttackType.PHYSICAL]
-
-        self.dmg = dmg
-        self.crt = crt
-        self.acc = acc
-        self.types = types
-
-        if atk_str is None:
-            atk_str = LString("Unspecified attack landed.")
-        self.atk_str = atk_str
-
-
 class WeaknessSet:
     def __init__(self, str_dict: dict[str, float]) -> None:
         weaknesses = {attack_type: 1.0 for attack_type in AttackType}
@@ -54,10 +32,32 @@ class WeaknessSet:
     def __getitem__(self, weakness: AttackType) -> float:
         return self.weaknesses[weakness]
 
-    def attack_factor(self, attack: Attack) -> float:
+    def attack_factor(self, attack: "Attack") -> float:
         factor = 1.0
 
         for attack_type in attack.types:
             factor *= self.weaknesses[attack_type]
 
         return factor
+
+    class Attack:
+        def __init__(self,
+                     dmg: int,
+                     acc: int = 0,
+                     crt: float = 1.0,
+                     types: list[AttackType] = None,
+                     atk_str: LString | None = None
+                     ) -> None:
+
+            if types is None:
+                types = [AttackType.PHYSICAL]
+
+            self.dmg = dmg
+            self.crt = crt
+            self.acc = acc
+            self.types = types
+
+            if atk_str is None:
+                atk_str = LString("Unspecified attack landed.")
+            self.atk_str = atk_str
+
