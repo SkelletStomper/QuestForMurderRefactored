eae4e47ab60516316db4bbe0bd73a8aeaaff46e5
stolhaufs <ole.haufschild@stud.h-da.de>
2024-08-07 08:50:03 +0200

    logging: logging for combat basics

--- src/combat/combat_basics.py
+++ src/combat/combat_basics.py
@@ -1,17 +1,29 @@
 from src.combat.attack import Attack
 
 
+import logging
+logger = logging.getLogger(__name__)
+
+
 def attack_dodged(accuracy: int, dodge: int) -> bool:
     from src.util.dice import d
     dice_dodge = d(8)
     if dice_dodge == 1:
+        logger.debug("Attack hit on critical dodge blunder")
         return False
     if dice_dodge == 8:
+        logger.debug("Attack missed on critical dodge success")
         return True
 
     dice_accuracy = d(6)
 
-    return (accuracy + dice_accuracy) < (dodge + dice_dodge - 1)
+    total_accuracy = accuracy + dice_accuracy
+    total_dodge = dodge + dice_dodge - 1
+    result = total_accuracy < total_dodge
+
+    logger.debug(f"Attack dodged: {result} (({accuracy} + {dice_accuracy} = {total_accuracy}) accuracy vs. ({dodge} + {dice_dodge} = {total_dodge}) dodge)")
+
+    return result
 
 
 class Combatant:
