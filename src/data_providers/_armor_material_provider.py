df19d8ea7d628b9b02c68080b213fa3fc4c813e9
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-09 12:46:21 +0200

    feat: implemented ArmorMaterialProvider

---
+++ src/data_providers/_armor_material_provider.py
@@ -0,0 +1,15 @@
+from src.items.armor import ArmorMaterial
+
+
+class ArmorMaterialProvider:
+    def __init__(self, armat_dict: dict[str, dict]):
+        self.armats: dict[str, ArmorMaterial] = {}
+
+        for armat_id, armat_data in armat_dict.items():
+            self.armats[armat_id] = ArmorMaterial(armat_id, armat_data)
+
+    def __getitem__(self, item: str):
+        return self.armats[item]
+
+    def __repr__(self):
+        return "ArmorMaterialProvider"
