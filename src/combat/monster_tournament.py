673a63259311154ed122280584e126021019c086
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-11 12:03:01 +0200

    feat: Bracketing

--- src/combat/monster_tournament.py
+++ src/combat/monster_tournament.py
@@ -22,20 +22,35 @@ class MonsterTournament:
             m1 = MonsterCombatant(self.c1)
             m2 = MonsterCombatant(self.c2)
 
-            winner = AutoCombat(m1, m2)
+            winner: Monster = AutoCombat(m1, m2).combat()  # type: ignore
             return winner
 
-
-
-
-
+        def __repr__(self) -> str:
+            if isinstance(self.c1, Monster):
+                print1 = self.c1.name
+            else:
+                print1 = self.c2
+            if isinstance(self.c2, Monster):
+                print2 = self.c2.name
+            else:
+                print2 = self.c2
+            return f"Bracket({print1}, {print2})"
 
     def __init__(self, rounds=3):
-
         monster_count = 2**rounds
         all_monsters = mp.all_ids()
 
         fighting_monsters = sample(all_monsters, monster_count)
+        fighting_monsters = [mp[monster] for monster in fighting_monsters]
+
+        brackets = [self.Bracket(fighting_monsters[i], fighting_monsters[i+1])
+                    for i in range(0, len(fighting_monsters), 2)]
+
+        while len(brackets) > 1:
+            brackets = [self.Bracket(brackets[i], brackets[i+1])
+                        for i in range(0, len(brackets), 2)]
+
+        print(brackets)
 
 
 
