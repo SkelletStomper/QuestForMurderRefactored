5ce708b7a3fec9e56ebb9fb902b0174fafa12952
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-11 18:28:15 +0200

    feat: species name
    Added a name to every species in plain, printable text.

--- src/entities/species.py
+++ src/entities/species.py
@@ -41,6 +41,8 @@ class Species:
         if self.subspecies_of == self.id:
             raise ValueError(f"Identical species name and Subspecies of in {self.id}")
 
+
+        self.name = in_dict["name"]
         self.description = in_dict["description"]
 
         self.anatomy = in_dict["anatomy"]
