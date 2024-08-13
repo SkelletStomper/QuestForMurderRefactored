3d1f83335570ee19bf107c66a5f0e6bb872532f2
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-13 18:50:52 +0200

    fix: Crash when not selecting an attack

--- src/combat/player_combatant.py
+++ src/combat/player_combatant.py
@@ -21,24 +21,30 @@ class PlayerCombatant(NPCCombatant):
         self.npc = npc
 
     def turn(self) -> tuple[PlayerCombatChoice, Any]:
-        print("What do you want to do?")
-        print("(1): Attack")
-        print("(2): Inventory")
-        print("(3): Flee")
-        choice: str = input(">: ").lower()
 
-        if choice in ["1", "(1)", "atk", "attack"]:
-            attacks = self.attack_dialogue()
-            if attacks is not None:
-                return PlayerCombatChoice.ATTACKING, attacks
-        elif choice in ["2", "(2)", "inv", "inventory"]:
-            took_turn = InventoryDialogue(self.npc.inventory, timed=True).dialogue()
-            if took_turn:
-                return PlayerCombatChoice.INT_ACTION, None
-        elif choice in ["3", "{3}", "flee", "run"]:
-            return PlayerCombatChoice.FLEEING, None
 
-    def attack_dialogue(self) -> list[Attack]|None:
+        while True:
+            print("What do you want to do?")
+            print("(1): Attack")
+            print("(2): Inventory")
+            print("(3): Flee")
+            choice: str = input(">: ").lower()
+            if choice in ["1", "(1)", "atk", "attack"]:
+                attacks = self.attack_dialogue()
+                if attacks is not None:
+                    return PlayerCombatChoice.ATTACKING, attacks
+            elif choice in ["2", "(2)", "inv", "inventory"]:
+                took_turn = InventoryDialogue(self.npc.inventory, timed=True).dialogue()
+                if took_turn:
+                    return PlayerCombatChoice.INT_ACTION, None
+            elif choice in ["3", "{3}", "flee", "run"]:
+                return PlayerCombatChoice.FLEEING, None
+            else:
+                print("Please make a valid choice.")
+
+
+
+    def attack_dialogue(self) -> list[Attack] | None:
         equip_effects = self.npc.inventory.calculate_bonus()
         stencils = equip_effects.granted_attacks
         player_input = ""
@@ -49,7 +55,8 @@ class PlayerCombatant(NPCCombatant):
             print("(0): Back")
             player_input = input(">:")
             index = int(player_input)-1
-            if 0 >= index < len(stencils):
+            print(f"index: {index}")
+            if 0 <= index < len(stencils):
                 return [stencils[index].generate_attack()]
 
         return None
