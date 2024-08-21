497378bdc83ffea8b58ee9f669eee8cb251c3a93
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-21 19:23:27 +0200

    feat: adjustable query prompt

--- src/dialogue/dialogue_query.py
+++ src/dialogue/dialogue_query.py
@@ -5,12 +5,13 @@ class DialogueQuery(Dialogue):
     def __init__(self, dialogue_id: str, in_dict: dict):
         super().__init__(dialogue_id, in_dict)
         self.input_query: str = in_dict["input"]
+        self.query_prompt: str = in_dict["query_prompt"]
         self.follow_up: str = in_dict["follow_up"]
 
     def play(self, dc: DialogueContext) -> str:
         super().play(dc)
 
-        player_input = input(">: ")
+        player_input = input(self.query_prompt)
 
         dc.add_context_variable(self.input_query, player_input)
 
