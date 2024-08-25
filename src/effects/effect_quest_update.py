40cd31bfa6b8cb2b571df0bc03f79f40c0281c65
Olivia Haufschild <SkelletStomper@gmx.de>
2024-08-25 22:00:44 +0200

    feat: effect for questlog updates

---
+++ src/effects/effect_quest_update.py
@@ -0,0 +1,17 @@
+from src.effects.effect_base import Effect
+
+from src.state.gamestate import global_state as gs
+
+
+class EffectQuestUpdate(Effect):
+    keyword = "quest_update"
+
+    def execute(self) -> None:
+        quest_log = gs.get_quest_log()
+        quest_id = self.parameters[0]
+        stage_id = self.parameters[1]
+
+        quest_log.set_quest_stage(quest_id, stage_id)
+
+
+
