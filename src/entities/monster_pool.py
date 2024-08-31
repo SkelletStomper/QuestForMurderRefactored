2d67eacc678958d91254b2282eeda6414c77c3c4
bolivia <skelletstomper@gmx.de>
2024-08-31 11:49:11 +0200

    feat: Monster Pool Class

---
+++ src/entities/monster_pool.py
@@ -0,0 +1,17 @@
+from src.entities.monster import Monster
+
+import random
+
+class MonsterPool:
+    def __init__(self, monster_pool_id: str, init_dict: dict):
+        self.monster_pool_id = monster_pool_id
+        self.monsters = init_dict["monsters"]
+
+
+    def random_monster(self) -> Monster:
+        from src.data_providers import monster_provider as mp
+        monster_id = random.choice(self.monsters)
+
+        return mp[monster_id]
+
+
