555ba6069c291ebeddbbfb40b7faa37c9c70d6ff
bolivia <skelletstomper@gmx.de>
2024-09-03 08:45:21 +0200

    feat: Location Class

---
+++ src/travel/location.py
@@ -0,0 +1,44 @@
+
+
+
+class Location:
+    class Interactive:
+        def __init__(self, interactive_id: str, in_dict: dict):
+            self.interactive_id = interactive_id
+            self.name: str = in_dict['name']
+            self.description: str = in_dict['description']
+            self.conditions: list[str] = in_dict['conditions']
+            self.effects: list[str] = in_dict['effects']
+
+    class Exit:
+        def __init__(self, exit_id: str, in_dict: dict):
+            self.exit_id = exit_id
+            self.name: str = in_dict['name']
+            self.description: str = in_dict['description']
+            self.conditions: list[str] = in_dict['conditions']
+            self.leads_to: str = in_dict['leads_to']
+
+
+
+    def __init__(self, location_id: str, in_dict: dict):
+        from src.data_providers import monster_pool_provider as mpp
+        from src.entities.monster_pool import MonsterPool
+
+        self.location_id = location_id
+        self.name: str = in_dict['name']
+        self.description: str = in_dict['description']
+        self.monster_pool: MonsterPool = mpp[in_dict['monster_pool']]
+
+        self.interactions: list[Location.Interactive] = [
+            Location.Interactive(interactive_id, interactive_data)
+            for interactive_id, interactive_data in in_dict["interactions"].items()
+        ]
+
+        self.exits: list[Location.Exit] = [
+            Location.Exit(exit_id, exit_data) for exit_id, exit_data in in_dict["exits"].items()
+        ]
+
+
+    def __repr__(self):
+        return (f"Location(id={self.location_id}, name={self.name}, description={self.description}, "
+                f"{len(self.interactions)} interactions, {len(self.exits)})")
