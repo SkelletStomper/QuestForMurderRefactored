50a6214f46628ab368091484d987d09745fa30a5
bolivia <skelletstomper@gmx.de>
2024-09-09 14:25:14 +0200

    feat: working travel dialogue

--- src/travel/travel_dialogue.py
+++ src/travel/travel_dialogue.py
@@ -1,5 +1,6 @@
 from src.state.gamestate import global_state as gs
 
+from src.travel.location import Location
 from src.effects import apply_effects
 
 class TravelDialogue:
@@ -21,12 +22,41 @@ class TravelDialogue:
             player_input = input(">: ").lower()
 
             if player_input in ["1", "(1)", "fight"]:
-                pass
+                if location.monster_pool.empty():
+                    print("It seems like there are no monsters at this location...")
+                else:
+                    gs.start_monster_fight(location.get_monster_id())
             elif player_input in ["2", "(2)", "interact"]:
-                pass
+                self.interact_dialogue(location.interactions, item_name="Interactions")
             elif player_input in ["3", "(3)", "leave"]:
-                pass
+                self.interact_dialogue(location.exits, item_name="Exits")
             elif player_input in ["0", "(0)", "back"]:
-                break
+                return
             else:
                 print("Make a valid answer!")
+
+    @staticmethod
+    def interact_dialogue(interactive_list: list[Location.Interactive], item_name):
+
+
+        while True:
+            filtered_interactive_list = [interactive for interactive in interactive_list if interactive.is_availaible()]
+
+            print(item_name + ":")
+            for i in range(len(filtered_interactive_list)):
+                print(f"({i+1}): {filtered_interactive_list[i].name}")
+
+            if len(filtered_interactive_list) == 0:
+                print("There are currently no available " + item_name)
+
+            print("(0): Back")
+            player_input = input(">: ").lower()
+
+            if player_input.isdigit():
+                index = int(player_input)-1
+                if 0 <= index < len(filtered_interactive_list):
+                    filtered_interactive_list[index].apply_effects()
+                    return
+                if index == -1:
+                    return
+            print("Make a valid answer!")
